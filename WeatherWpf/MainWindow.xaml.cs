5acfdb4056e61432b9a09c4220ca360584889ab6
vladislavv27 <vladislavv27@gmail.com>
2023-04-21 13:43:00 +0300

    add design

--- WeatherWpf/MainWindow.xaml.cs
+++ WeatherWpf/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,26 @@ namespace WeatherWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        string apiKey = "113d894b21400eacbab0da34887b802d";
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+
+
+        private void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            getweather();
+        }
+
+        private void getweather()
+        {
+           using(WebClient web=new WebClient())
+           {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", textboxCity.Text, apiKey);
+           }
         }
     }
 }
